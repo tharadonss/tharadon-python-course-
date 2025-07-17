7a6ad253e27dcfaadea8b0b2eb05c550114e94c5
Tharadon Suesat <tharadon.su@ku.th>
2025-07-17 14:50:46 +0700

    https://github.com/tharadonss/python-programming-course.git

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,23 @@
+# Exercise 1: Personal Profile Creator
+print("=== Personal Profile Creator ===")
+# Create a program that asks for:
+# - Full name
+# - Age
+# - Email
+# - Phone number
+# - Favorite hobby
+# Then display it as a profile
+
+# Write your solution here:
+full_name = input("What is your name? :")
+age = input("how old are you? :")
+email = input("What is your email address? :")
+phon_number = input("What is your phone number? :")
+hobby = input("What is your favorite hobby? :")
+
+print("=== Your profile ===")
+print("Full name: ", full_name)
+print("Age: ", age)
+print("Email: ", email)
+print("Phone Number: ", phone_number)
+print("Favorite Hobby: ", hobby)
